23497cd70ece514809d17cb076b1ae18f566b6d4
Nobutann <luizgonzagafilho201365@gmail.com>
2025-09-02 20:56:02 -0300

    creating scripts for bullet and spawner

---
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletLife = 1f;
+    public float rotation = 0f;
+    public float speed = 0f;
+
+    private Vector2 spawnPoint;
+    private float timer = 0f;
+
+    void Start()
+    {
+        spawnPoint = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    void Update()
+    {
+        if (timer > bulletLife) Destroy(this.gameObject);
+        timer += Time.deltaTime;
+        transform.position = Movement(timer);
+    }
+    
+    private Vector2 Movement(float timer)
+    {
+        float x = timer * speed * transform.right.x;
+        float y = timer * speed * transform.right.y;
+
+        return new Vector2(x + spawnPoint.x, y + spawnPoint.y);
+    }
+}
